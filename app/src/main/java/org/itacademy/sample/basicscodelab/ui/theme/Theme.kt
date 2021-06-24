cac461713c929eb17b4aecdb98608a1df74bcaab
Siarhei Naralenkau <siarhei.naralenkau@godel.shellenergy.co.uk>
2021-06-24 16:55:11 +0300

    Codelab implementation

---
+++ app/src/main/java/org/itacademy/sample/basicscodelab/ui/theme/Theme.kt
@@ -0,0 +1,57 @@
+package org.itacademy.sample.basicscodelab.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = purple200,
+    primaryVariant = purple700,
+    secondary = teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = purple500,
+    primaryVariant = purple700,
+    secondary = teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+private val DarkColors = darkColors(
+    primary = purple200,
+    primaryVariant = purple700,
+    secondary = teal200
+)
+
+private val LightColors = lightColors(
+    primary = purple500,
+    primaryVariant = purple700,
+    secondary = teal200
+)
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+    MaterialTheme(colors = colors) {
+        content()
+    }
+
+}
